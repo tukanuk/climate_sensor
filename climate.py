83e616ad9f9c2335b97350f1831164e9f451d0fe
tukanuk <benjamin@idavidson.ca>
2021-08-29 02:36:50 +0100

    writes to json

--- climate.py
+++ climate.py
@@ -1,14 +1,59 @@
 import Adafruit_DHT
 import time
- 
+import sys
+from datetime import datetime
+import json
+
 DHT_SENSOR = Adafruit_DHT.DHT11
 DHT_PIN = 4
+
+opts = [opt for opt in sys.argv[1:] if opt.startswith("-")]
+args = [arg for arg in sys.argv[1:] if not arg.startswith("-")]
+
+if "-l" in opts:
+    location = args[0]
+    print(f"The location is: {location}")
+
+print(opts)
+print(args)
+
+ClimateRecords = []
+
+def write_json(new_record, filename="climate_data.json"):
+    with open(filename, "r+") as fp:
+        file_data = json.load(fp)
+        file_data.append(new_record)
+        fp.seek(0)
+        json.dump(file_data, fp, indent=4)
+        
+
+class ClimateRecord:
+
+    def __init__(self, location, temperature, humidity):
+        self.timestamp = datetime.now()
+        self.location = location
+        self.temperature = temperature
+        self.humidity = humidity
+
+    def output(self):
+        print(f"Time: {self.timestamp}, Location: {self.location}, Temp: {self.temperature}C, Humidity: {self.humidity}%")
+    
+    def json(self):
+        return({"timestamp": str(self.timestamp), "location": self.location, "temperature": self.temperature, "humidity": self.humidity})
+
+
  
 while True:
     humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
     if humidity is not None and temperature is not None:
-        print("Temp={0:0.1f}C Humidity={1:0.1f}%".format(temperature, humidity))
+        # print(f"Temp={temperature}C Humidity={humidity:0.1f}%")
+        temp = ClimateRecord(location, temperature, humidity) 
+        ClimateRecords.append(temp)
+        temp.output()
+        print(temp.json())
+        write_json(temp.json())
+
     else:
         print("Sensor failure. Check wiring.");
-    time.sleep(3);
+    time.sleep(10);
  
